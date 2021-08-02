d3244ff468d98e133afe780e32cfec34325ffb29
Pedro <pedro07_machado@hotmail.com>
2021-08-02 19:39:33 -0300

    commit 1

---
+++ git.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+	int idade;
+	string n;
+	
+	cout << "Seja Bem Vindo" <<  endl ;
+	cout << "Digite sua idade : ";
+	cin >> idade;
+}
